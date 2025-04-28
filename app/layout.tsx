02b66ead114e9d7976ba94780ab32d2a7163095b
Danial Jelvani <jelvanidanial@gmail.com>
2025-04-28 20:10:00 +0330

    add footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "@/assets/styles/globals.css";
 import Navbar from "@/components/navbar";
-import isf from "@/assets/images/isfahan1.png";
+import Footer from "@/components/footer";
 
 export const metadata = {
   title: "Isfahan Traditional Rentals",
@@ -26,11 +26,12 @@ const MainLayout = ({ children }: { children: React.ReactNode }) => {
         className="absolute inset-0 bg-cover bg-fixed bg-no-repeat bg-center"
         style={{ backgroundImage: "url('images/background.png')" }}
       >
-        <div className="absolute inset-0 bg-gradient-to-b from-neutral-800/70 via-neutral-800/60 to-neutral-800/40 opacity-80" />
+        <div className="absolute inset-0 bg-gradient-to-b from-neutral-800/70 via-neutral-800/60 to-neutral-800/40 opacity-80"></div>
 
-        <div className="relative z-10">
+        <div className="relative z-10 min-h-screen flex flex-col">
           <Navbar />
-          <main>{children}</main>
+          <main className="flex-grow">{children}</main>
+          <Footer />
         </div>
       </body>
     </html>
---
+++ components/footer.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Image from "next/image";
+import logo from "@/assets/images/logo.png";
+
+const footer = () => {
+  return (
+    <footer className="bg-black/40 w-full py-4">
+      <div className="container mx-auto flex flex-col md:flex-row items-center justify-around px-4">
+        <div className="mb-2 md:mb-0">
+          <Image src={logo} alt="Logo" className="h-8 w-auto" />
+        </div>
+        <div>
+          <p className="text-sm text-gray-500 mt-2 md:mt-0">
+            &copy; 2024 Iran Traditional Rentals. All rights reserved.
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default footer;
