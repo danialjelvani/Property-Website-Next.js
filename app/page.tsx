0818530eed93c36cd5efd0caac1f97ea0a53e048
Danial Jelvani <jelvanidanial@gmail.com>
2025-05-02 00:06:08 +0330

    add homeproperties

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "@/components/hero";
 import InfoBoxes from "@/components/infoBoxes"
+import PropertiesPage from "@/components/homeProperties"
 
 const HomePage = () => {
   return (
     <>
       <Hero />
       <InfoBoxes />
+      <PropertiesPage />
     </>
   );
 };
---
+++ components/homeProperties.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import Link from "next/link";
+import properties from "@/properties.json";
+import PropertyCard from "@/components/PropertyCard";
+
+const homeProperties = () => {
+  const randomProperties = properties
+    .sort(() => Math.random() - Math.random())
+    .slice(0, 3);
+  return (
+    <>
+      <section className="px-4 py-6">
+        <div className="container m-auto">
+          <h2
+            style={{ textShadow: "0px 0px 10px rgba(0, 0, 0, 0.9)" }}
+            className="text-4xl font-Title2 tracking-wider font-extrabold text-teal-300 mt-8 mb-12 text-center"
+          >
+            Properties
+          </h2>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {randomProperties.length === 0 ? (
+              <p>No properties found.</p>
+            ) : (
+              randomProperties.map((property) => (
+                <PropertyCard key={property._id} property={property} />
+              ))
+            )}
+          </div>
+        </div>
+      </section>
+      <section className="mx-auto max-w-lg my-6 px-6">
+        <Link
+          href="/properties"
+          className="block linkbuttonemerald text-white text-center py-4 px-6 rounded-xl"
+        >
+          View All Properties
+        </Link>
+      </section>
+    </>
+  );
+};
+
+export default homeProperties;
